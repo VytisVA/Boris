dc8187056412e98a3b99273a9410bdc8fc7f9da8
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-21 14:25:41 +0000

    bike.rb bike_spec.rb setup

---
+++ spec/bike_spec.rb
@@ -0,0 +1,9 @@
+require 'bike'
+
+describe Bike do
+
+  # describe "release_bike"
+  # it "responds to release_bike method" do
+  #   expect(DockingStation.new).to respond_to (:release_bike)
+  # end
+end
