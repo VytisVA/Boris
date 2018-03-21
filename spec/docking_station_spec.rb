9a10a6b99dc585c38eb0238087bc6c2b977efecd
Vytis Vadoklis <vytis.vadoklis@outlook.com>
2018-03-21 15:02:51 +0000

    step 10 complete

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,8 @@ describe DockingStation do
   it "responds to release_bike method" do
     expect(DockingStation.new).to respond_to (:release_bike)
   end
+
+  it "it creates a bike" do
+    expect(DockingStation.new.release_bike).to be_instance_of (Bike)
+  end
 end
